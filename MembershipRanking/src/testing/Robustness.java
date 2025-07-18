872e9f30d1e741449de4e5a0929e4f9ead2058ba
Adithep <adithep.ma@kkumail.com>
2025-07-18 23:02:07 +0700

    Lab 4

---
+++ MembershipRanking/src/testing/Robustness.java
@@ -0,0 +1,50 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import sqa.main.Ranking;
+
+
+class Robustness {
+
+	Ranking r;
+	
+	@BeforeEach
+	public void setUp() throws Exception{
+		r = new Ranking();
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+
+        "-1, 2, 100, Invalid Input",
+        "0, 2, 100, Standard",
+        "10000, 2, 100, Silver",
+        "99999, 5, 500, Gold",
+        "1000000, 6, 1000, Platinum",
+
+        "10000, -1, 100, Invalid Input",
+        "10000, 0, 100, Standard",
+        "10000, 2, 100, Silver",
+        "50000, 5, 500, Gold",
+        "100000, 8, 1000, Standard",
+
+        "10000, 2, -1, Invalid Input",
+        "10000, 2, 0, Standard",
+        "10000, 2, 100, Silver",
+        "50000, 5, 500, Gold",
+        "100000, 6, 10000, Platinum",
+
+        "9999999, 10, 99999, Platinum",
+        "100000, 8, 1000, Standard"
+		
+	})
+	
+	void robustTest(int total, int freq, int point, String expected) {
+		assertEquals(expected, r.CalculateMembershipRank(total, freq, point));
+	}
+}
---
+++ MembershipRanking/src/testing/boundary.java
@@ -0,0 +1,37 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import sqa.main.Ranking;
+
+class Boundary {
+
+    Ranking r;
+    
+    @BeforeEach
+    public void setUp() throws Exception{
+    	r = new Ranking();
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+    	"999, 1, 99, Standard",
+    	"9999, 1, 99, Standard",
+    	"50000, 1, 500, Standard",
+    	"50000, 6, 500, Standard",
+    	"50000, 7, 500, Standard",
+    	"49999,	1, 500, Silver",
+    	"50000, 1, 500, Silver",
+    	"75000,	4, 750, Gold",
+    	"100000, 4, 500, Gold",
+    	"100000, 6, 1000, Platinum"
+    	
+    })
+    void boundaryTest(int purchaseTotal, int freq, int point, String expected) {
+    	assertEquals(expected, r.CalculateMembershipRank(purchaseTotal, freq, point));
+    }
+}
